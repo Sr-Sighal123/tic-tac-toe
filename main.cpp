47229e251cab91fa6d72fe8cda2cc2a8ad0307eb
Shristi Singhal <ss7377@srmist.edu.in>
2026-04-27 21:42:20 +0530

    UC5: Move validation

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
-// UC1: Display empty Tic Tac Toe board
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
-
 void initializeBoard(char board[3][3]) {
     for(int i=0;i<3;i++)
         for(int j=0;j<3;j++)
@@ -17,11 +17,64 @@ void printBoard(char board[3][3]) {
     }
 }
 
+void toss(char &currentPlayer, char &p1Symbol, char &p2Symbol) {
+    srand(time(0));
+    int result = rand() % 2;
+
+    if(result == 0) {
+        currentPlayer = '1';
+        p1Symbol = 'X';
+        p2Symbol = 'O';
+        cout << "Player 1 starts and is X" << endl;
+    } else {
+        currentPlayer = '2';
+        p2Symbol = 'X';
+        p1Symbol = 'O';
+        cout << "Player 2 starts and is X" << endl;
+    }
+}
+
+int getUserInput() {
+    int slot;
+    cout << "Enter slot (1-9): ";
+    cin >> slot;
+    return slot;
+}
+
+void convertToIndex(int slot, int &row, int &col) {
+    row = (slot - 1) / 3;
+    col = (slot - 1) % 3;
+}
+
+bool isValidMove(char board[3][3], int row, int col) {
+    if(row < 0 || row > 2 || col < 0 || col > 2)
+        return false;
+
+    if(board[row][col] != '-')
+        return false;
+
+    return true;
+}
+
 int main() {
     char board[3][3];
+    char currentPlayer, p1Symbol, p2Symbol;
 
     initializeBoard(board);
     printBoard(board);
+
+    toss(currentPlayer, p1Symbol, p2Symbol);
+
+    int slot = getUserInput();
+
+    int row, col;
+    convertToIndex(slot, row, col);
+
+    if(isValidMove(board, row, col)) {
+        cout << "Valid Move!" << endl;
+    } else {
+        cout << "Invalid Move!" << endl;
+    }
 
     return 0;
 }
