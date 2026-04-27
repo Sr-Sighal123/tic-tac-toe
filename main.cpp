f903f74db56a81f3a0d0eb65c84aaacf5c56f57e
Shristi Singhal <ss7377@srmist.edu.in>
2026-04-27 21:04:59 +0530

    UC1: Display empty board

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void initializeBoard(char board[3][3]) {
+    for(int i=0;i<3;i++)
+        for(int j=0;j<3;j++)
+            board[i][j] = '-';
+}
+
+void printBoard(char board[3][3]) {
+    for(int i=0;i<3;i++) {
+        for(int j=0;j<3;j++) {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    char board[3][3];
+
+    initializeBoard(board);
+    printBoard(board);
+
+    return 0;
+}
